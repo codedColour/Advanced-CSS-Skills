d77800d9467f5c7cd2d6b3f5c4a1244dc1fe5363
codedColour <{61967108}+{codedColour}@users.noreply.github.com>
2021-02-16 18:10:08 +0000

    Fully Responsive

--- _scripts/myScripts.js
+++ _scripts/myScripts.js
@@ -1,54 +1,56 @@
-// Back To Top Button Functionality
+window.onload = function () {
+    // Back To Top Button Functionality
 
-var backToTopButton = document.querySelector('.btn-scroll-to-top');
+    var backToTopButton = document.querySelector('.btn-scroll-to-top');
 
-window.addEventListener('scroll', makeVisible);
+    window.addEventListener('scroll', makeVisible);
 
-function makeVisible() {
-    if (window.pageYOffset > 2500) {
-        if (!backToTopButton.classList.contains('btnEntrance')) {
-            backToTopButton.classList.remove('btnExit');
-            backToTopButton.classList.add('btnEntrance');
-            backToTopButton.style.display = 'block';
-        }
-    } else {
-        if (backToTopButton.classList.contains('btnEntrance')) {
-            backToTopButton.classList.remove('btnEntrance');
-            backToTopButton.classList.add('btnExit');
+    function makeVisible() {
+        if (window.pageYOffset > 2500) {
+            if (!backToTopButton.classList.contains('btnEntrance')) {
+                backToTopButton.classList.remove('btnExit');
+                backToTopButton.classList.add('btnEntrance');
+                backToTopButton.style.display = 'block';
+            }
+        } else {
+            if (backToTopButton.classList.contains('btnEntrance')) {
+                backToTopButton.classList.remove('btnEntrance');
+                backToTopButton.classList.add('btnExit');
 
-            setTimeout(function() {
-              backToTopButton.style.display = 'none';
-            }, 250);            
+                setTimeout(function () {
+                    backToTopButton.style.display = 'none';
+                }, 250);
+            }
         }
     }
-}
 
-backToTopButton.addEventListener('click', smoothScrollBackToTop);
+    backToTopButton.addEventListener('click', smoothScrollBackToTop);
 
+    // Smooth scrolling effect
+    function smoothScrollBackToTop() {
+        const targetPosition = 0;
+        const startPosition = window.pageYOffset;
+        const distance = targetPosition - startPosition;
+        const duration = 750;
+        let start = null;
 
-// Smooth scrolling effect
-function smoothScrollBackToTop() {
-  const targetPosition = 0;
-  const startPosition = window.pageYOffset;
-  const distance = targetPosition - startPosition;
-  const duration = 750;
-  let start = null;
+        window.requestAnimationFrame(step);
 
-  window.requestAnimationFrame(step);
+        function step(timestamp) {
+            if (!start) start = timestamp;
+            const progress = timestamp - start;
+            window.scrollTo(
+                0,
+                easeInOutCubic(progress, startPosition, distance, duration),
+            );
+            if (progress < duration) window.requestAnimationFrame(step);
+        }
+    }
 
-  function step(timestamp) {
-
-    if (!start) start = timestamp;
-    const progress = timestamp - start;
-    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
-    if (progress < duration) window.requestAnimationFrame(step);
-  }
-}
-
-function easeInOutCubic(t, b, c, d) {
-
-  t /= d/2;
-  if (t < 1) return c/2*t*t*t + b;
-  t -= 2;
-  return c/2*(t*t*t + 2) + b;
+    function easeInOutCubic(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return (c / 2) * t * t * t + b;
+        t -= 2;
+        return (c / 2) * (t * t * t + 2) + b;
+    }
 };
